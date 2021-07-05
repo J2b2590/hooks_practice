7dd03980b683425457aeb58449e0cb2adb54af49
J2b2590 <J2b2590@gmail.com>
2021-07-04 20:47:15 -0500

    clear timer

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>UP</button>
       <button onClick={() => setCount(count - 1)}>DOWN</button>
+      <button onClick={() => setCount(count === 0)}>CLEAR</button>
     </div>
   );
 }
